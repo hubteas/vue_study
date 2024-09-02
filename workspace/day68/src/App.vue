1e1da6916c5a086eac8da628fbe8366739346317
hubteas <tykim888@naver.com>
2024-09-02 21:56:29 +0900

    20240902

---
+++ workspace/day68/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="increment">카운트 : {{ counter }}</button>
+  </div>
+</template>
+
+<script>
+import{ onMounted, ref} from 'vue';
+
+export default{
+  setup(){
+    const counter = ref(0);
+
+    const increment = ()=> counter.value++;
+
+    onMounted(()=>{
+      console.log("마운트되었습니다");
+    });
+    return{
+      counter,
+      increment,
+    };
+  },
+}
+
+</script>
+
+
